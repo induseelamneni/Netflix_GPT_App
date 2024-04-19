8d8bc0b8bdad2dbbad7b431350c8c6dc1da4a297
induseelamneni <sivaindu222@gmail.com>
2024-04-19 09:36:45 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './App.css';
 import Body from './components/Body';
 import { Provider } from 'react-redux';
@@ -12,6 +13,29 @@ function App() {
      </Provider>
   
     // </div>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> Initialize project using Create React App
   );
 }
 
